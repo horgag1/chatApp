cc0978ad911ace8ec852a933d35c50a4728f7232
GERARD HORGAN <gerard.horgan1@gmail.com>
2017-01-16 12:01:38 +0000

    push chat app to github

---
+++ app/src/main/java/com/gerhorgan/friendlychat1/CodelabPreferences.java
@@ -0,0 +1,11 @@
+package com.gerhorgan.friendlychat1;
+
+/**
+ * Created by gerhorgan on 16/01/2017.
+ */
+
+public class CodelabPreferences {
+
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
